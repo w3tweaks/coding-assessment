c0cd23ec2b5b9fb8392b1f3efc52db6b96a5a549
Brandon Taylor <btaylor@brandon-mbp.lan>
2019-12-05 11:53:49 -0500

    Added initial markup and styling. Added ngrx store and store-devtools. Added paths to tsconfig.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { environment } from '@app/environments/environment';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot([], {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument({
+      maxAge: 25,
+    }) : [],
   ],
   providers: [],
   bootstrap: [AppComponent]
